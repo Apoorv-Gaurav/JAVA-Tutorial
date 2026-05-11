431fee900a56b1a4bd9e74d819f0969911b1401a
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2026-05-11 13:28:53 +0530

    Scanner done

---
+++ day2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class day2 {
+    public static void main(String args[]){
+        Scanner scan  = new Scanner(System.in);
+        System.out.print("Enter the name: ");
+        String name = scan.nextLine();
+
+        int age = scan.nextInt();
+
+        System.out.println("Hello " + name);
+        System.out.println("Age: "+ age);
+
+        //Common Issue
+
+        System.out.println("Enter colour: ");
+        //---------------------
+        scan.nextLine();
+        //------------------
+        String col = scan.nextLine();
+        System.out.println("Color: "+col);
+        scan.close();
+    }
+}
