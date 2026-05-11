16c8ebad88025cafac7b8b5fec8da8fe2e1e6e36
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2026-05-11 13:28:42 +0530

    basic start

---
+++ day1.java
@@ -0,0 +1,16 @@
+public class day1 {
+    /*Multi Line Comment */
+    //Single Line Comment
+    public static void main(String args[]){
+        System.out.println();
+        int num = 0;
+        double salary = 3202.22;
+        float avg = 2;
+        boolean isTrue = true;
+
+        char ch = 'd';
+
+        String sen = "hello  afsd";
+        System.out.println(sen + " " + ch + " " + isTrue + " " + avg + " " + salary + " " + num);
+    }
+}
