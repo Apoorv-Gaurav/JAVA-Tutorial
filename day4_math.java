9d38265452344dc0ac8ff912a8d4a4b1d1410e4b
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2026-05-31 18:35:43 +0530

    Math library done

---
+++ day4_math.java
@@ -0,0 +1,18 @@
+public class day4_math {
+    public static void main(String args[]){
+        //Math.max(5,10);
+        //Math.min(2,3);
+        //Math.sqrt(64);
+        //Math.abs(-32);
+        //Math.pow(2,3);  //8.0
+
+        //Math.round(4.6);  // 5
+        //Math.ceil(4.1);   // 5.0
+        //Math.floor(4.9);  // 4.0
+
+        //Math.random();
+        //int randomNum = (int)(Math.random() * 101);  // 0 to 100
+
+        System.out.println();
+    }
+}
