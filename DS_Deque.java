51feba7cba5550d8b52020471b80ce46bf74e650
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2026-06-05 09:54:41 +0530

    Deque in Java done

---
+++ DS_Deque.java
@@ -0,0 +1,26 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DS_Deque {
+    public static void main(String[] args)
+    {
+
+        // Create a Deque of Strings
+        Deque<String> d = new ArrayDeque<>();
+
+        d.addFirst("1");
+        d.addLast("2");
+
+        String f = d.removeFirst();
+        String l = d.removeLast();
+
+        // Displaying the Deque
+        System.out.println("First: " + f + ", Last: " + l);
+    }
+
+    //.addFirst();
+    //.addLast();
+    //.removeFirst();
+    //.removeLast();
+    
+}
